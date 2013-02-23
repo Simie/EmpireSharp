9c4a1ef20f664bb2e1a9049aab15263cc23482a1
Simon Moles <simon.moles@gmail.com>
2013-02-23 01:33:45 +0000

    Added license headers

--- EmpireSharp.Simulation/Time.cs
+++ EmpireSharp.Simulation/Time.cs
@@ -1,4 +1,12 @@
-﻿using FixMath.NET;
+﻿/*
+*  This Source Code Form is subject to the terms of the Mozilla Public
+*  License, v. 2.0. If a copy of the MPL was not distributed with this
+*  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+*  EmpireSharp (c) Simon Moles 2013 (www.simonmoles.com)
+*
+*/
+using FixMath.NET;
 
 namespace EmpireSharp.Simulation
 {
