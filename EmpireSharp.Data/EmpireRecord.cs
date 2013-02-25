d221a3d3f8fa87edcd5176f528ff7086997a2387
Simon Moles <simon.moles@gmail.com>
2013-02-25 23:05:01 +0000

    Added Data/Editor projects, based on Papyrus. Removed Papyrus submodule (using NuGet instead)

---
+++ EmpireSharp.Data/EmpireRecord.cs
@@ -0,0 +1,14 @@
+﻿using Papyrus;
+
+[assembly: Papyrus.PapyrusModule("8A1666A0-E60D-48B1-BF9E-9A27A6369DDE", typeof(EmpireSharp.Data.EmpireRecord))]
+
+namespace EmpireSharp.Data
+{
+
+	[Record(ShowInEditor = false)]
+	[ChildRecord(10, typeof(Unit))]
+	public class EmpireRecord : Papyrus.DataTypes.Record
+	{
+	}
+
+}
---
+++ EmpireSharp.Data/Unit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Papyrus;
+
+namespace EmpireSharp.Data
+{
+
+	[Record]
+	public class Unit : EmpireRecord
+	{
+
+		private string _name;
+
+		[RecordProperty(1)]
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				ThrowIfReadOnly("Name");
+				_name = value;
+				RaisePropertyChanged("Name");
+			}
+		}
+
+	}
+
+}
